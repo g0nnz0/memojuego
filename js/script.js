3897742ec8adfaa82a1c604d492c0d5166d87d99
g0nnz0 <bonzai_rock@hotmail.com>
2022-09-15 23:42:03 -0300

    creé la funcion reparteTarjetas()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+var grupoTarjetas = ["🎈", "🎆", "🎇", "🧨", "✨", "🎉", "🎊", "🎃"];
 
+var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
+
+function reparteTarjetas() {
+  var mesa = document.querySelector("#mesa");
+
+  mesa.innerHTML = "";
+
+  totalTarjetas.forEach(function (elemento) {
+    var tarjeta = document.createElement("div");
+
+    tarjeta.innerHTML =
+      "<div class='tarjeta'>" +
+      "<div class='tarjeta__contenido'>" +
+      elemento +
+      "</div>" +
+      "</div>";
+
+    mesa.appendChild(tarjeta);
+  });
+}
+
+reparteTarjetas();
